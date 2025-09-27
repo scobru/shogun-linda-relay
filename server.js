d5c2cec8442517bb0bef2371957d1c9d5324b7fe
Francesco Bruno <scobru1988@gmail.com>
2025-09-27 21:19:47 +0200

    remove cors

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const PORT = process.env.PORT || 8765;
 // Middleware
 app.use(helmet());
 app.use(compression());
-app.use(cors({
+/* app.use(cors({
   origin: process.env.FRONTEND_URL || 'https://linda.shogun-eco.xyz',
   credentials: true
-}));
+})); */
 app.use(express.json({ limit: '50mb' }));
 
 // SQLite database
